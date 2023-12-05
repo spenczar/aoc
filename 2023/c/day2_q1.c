0df74044f092fd6c732f9a2764180177e518857a
Spencer Nelson <s@spencerwnelson.com>
2023-12-05 07:15:59 -0800

    Refactor

--- 2023/c/day2_q1.c
+++ 2023/c/day2_q1.c
@@ -13,63 +13,61 @@ typedef struct Draw {
   long green;
 } Draw;
 
-int main(int argc, char * argv[]) {
-  FILE *inputf;
+int exceeds_max(char *line) {
+  char *chunk, *draw, *endptr;
+  long qty;
+  while ((chunk = strsep(&line, ";")) != NULL) {
+    char *chunk_copy = chunk;
+    while ((draw = strsep(&chunk_copy, ",")) != NULL) {
+      qty = strtol(draw, &endptr, 10);
+      switch (endptr[1]) {
+      case 'b':
+        if (qty > MAX_BLUE) {
+          return 1;
+        }
+        break;
+      case 'r':
+        if (qty > MAX_RED) {
+          return 1;
+        }
+        break;
+      case 'g':
+        if (qty > MAX_GREEN) {
+          return 1;
+        }
+        break;
+      default:
+        printf("ERROR! char is %c", endptr[1]);
+        return -1;
+        break;
+      }
+    }
+  }
+  return 0;
+}
 
+int main(int argc, char * argv[]) {
   if (argc != 2) {
     printf("missing input");
     return 1;
   }
+  FILE *inputf  = fopen(argv[1], "r");
 
-  inputf = fopen(argv[1], "r");
-
-  long total = 0;
-  int game_id;
+  long total = 0, game_id;
   char *line = NULL;
   size_t linecap = 0;
   ssize_t linelen;
-  char *chunk;
   while ((linelen = getline(&line, &linecap, inputf)) > 0) {
-    game_id = atoi(&line[5]);
+    game_id = strtol(&line[5], NULL, 10);
 
     char *line_copy = line;
     // Advance past the colon
     line_copy = strstr(line_copy, ":");
     line_copy = line_copy + 2;
 
-    while ((chunk = strsep(&line_copy, ";")) != NULL) {
-      char *draw;
-      char *endptr;
-      long qty;
-      while ((draw = strsep(&chunk, ",")) != NULL) {
-        qty = strtol(draw, &endptr, 10);
-        switch (endptr[1]) {
-        case 'b':
-          if (qty > MAX_BLUE) {
-            goto next_line;
-          }
-          break;
-        case 'r':
-          if (qty > MAX_RED) {
-            goto next_line;
-          }
-          break;
-        case 'g':
-          if (qty > MAX_GREEN) {
-            goto next_line;
-          }
-          break;
-        default:
-          printf("ERROR! char is %c", endptr[1]);
-          return 1;
-          break;
-        }
-      }
+    if (exceeds_max(line_copy) == 0) {
+      total += game_id;
     }
-    total += game_id;
- next_line:
-    // ??? noop to get the goto to work OK
-    printf("");
   }
   printf("\ntotal: %ld\n", total);
 }
